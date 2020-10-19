a76227121254fdc80f94b54923139d47e6b1bdbd
Andreas Hellerschmied <andreas.hellerschmied@bev.gv.at>
2020-10-19 13:06:09 +0200

    Cleaned project.

---
+++ gravtools/scripts/__init__.py
@@ -0,0 +1,52 @@
+'''
+Command line interface o gravtools.
+Author: Andreas Hellerschmied
+'''
+from gravtools.schwaus import main as schwaus_main
+import argparse
+import os
+from gravtools.options import out_path, path_oesgn_table, name_oesgn_table
+
+
+def is_file(filename):
+    """Check, whether the input string is the path to an existing file."""
+    if os.path.isfile(filename):
+        return filename
+    raise argparse.ArgumentTypeError("'{}' is not a valid file.".format(filename))
+
+
+def is_dir(pathname):
+    """Check, whether the input string is a valid and existing filepath."""
+    if os.path.exists(pathname):
+        return pathname
+    raise argparse.ArgumentTypeError("'{}' is not a valid directory.".format(pathname))
+
+
+def schwaus():
+    """Command line interface including argument parser for the legacy BEV gravity survey processing scheme."""
+
+    parser = argparse.ArgumentParser(prog="schwaus",
+                                     description="Legacy gravity survey processing scheme at BEV.",
+                                     epilog="The observatios are drift-corrected by fitting a polynomial of "
+                                            "degree 1 to 3 using multiple linear regression. In the second step, the "
+                                            "corrected observations are fitted onto the ÖSGN level.",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--obs-file", type=is_file, required=True, help="path and name of observation file")
+    parser.add_argument("--oesgn-table", required=False, type=is_file, help="path and name of ÖSGN table",
+                        default=path_oesgn_table + name_oesgn_table)
+    parser.add_argument("--out-dir", type=is_dir, required=False,
+                        help="path to output directory", default=out_path)
+    args = parser.parse_args()
+    # print(args)
+
+    # Prepare paths and file names for function call:
+    path_oesgn_table_str, name_oesgn_table_str = os.path.split(args.oesgn_table)
+    path_obs_file_str, name_obs_file_str = os.path.split(args.obs_file)
+    if path_oesgn_table_str:  # String not empty => Append '/'
+        path_oesgn_table_str += '/'
+    if path_obs_file_str:  # String not empty => Append '/'
+        path_obs_file_str += '/'
+    args.out_dir += '/'
+
+    return schwaus_main(path_oesgn_table_str, name_oesgn_table_str, path_obs_file_str, name_obs_file_str, args.out_dir)
+
